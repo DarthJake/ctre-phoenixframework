07f40d4a476774f794105fdf4ecb0a40087acb9d
DarthJake <metheamazingwebprogrammer@gmail.com>
2021-01-20 13:15:48 -0700

    Initial commit.

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url         = 'https://github.com/CrossTheRoadElec/Phoenix-Releases/releases/download/v5.19.4.1/CTRE_Phoenix_Framework_v5.19.4.1.exe'
+$fileName    = 'CTRE_Phoenix_Framework_v5.19.4.1.exe'
+$checksum = '1EA016D735A7002CB9CE7979006EE8B2B0D4BA953FC3E29898DD63D311D29B46'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  softwareName   = 'CTRE Phoenix Framework*'
+  fileType       = 'EXE'
+  Url            = $url
+  file           = $fileName
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0, 1223) # 1223 because there is a bug with the installer and /S right now
+}
+
+Install-ChocolateyPackage  @packageArgs #https://docs.chocolatey.org/en-us/guides/create/mount-an-iso-in-chocolatey-package
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'CTRE Phoenix Framework*'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  fileType      = 'EXE'
+  silentArgs   = '/S' # NSIS
+  validExitCodes= @(0) #please insert other valid exit codes here
+}
+
+Uninstall-ChocolateyPackage @packageArgs
